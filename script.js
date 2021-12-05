ada91e7741e98bd79ce78435d1935f38dd7277e5
Piotr Wojciechowski <piotr87654@gmail.com>
2021-12-05 19:10:27 +0000

    Added skills bar + form validation

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 $('document').ready(function(){
     var scrollSpy = new bootstrap.ScrollSpy(document.body, { target: '#navbar' })
-    $('#contactModal').on('shown.bs.modal', function () {
-        $('#name').trigger('focus')
-    })
-});
 
+    $('#contactModal').on('shown.bs.modal', function () {
+        $('#modalName').trigger('focus')
+      })
+
+    var forms = document.querySelectorAll('.needs-validation')
+    // Loop over form and prevent submission if input is incorrect
+    Array.prototype.slice.call(forms).forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            } else {
+                alert("Your message has been sent successfully!")
+            }
+            form.classList.add('was-validated')
+        }, false)
+    });
+});
