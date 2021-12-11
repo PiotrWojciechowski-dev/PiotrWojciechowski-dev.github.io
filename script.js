04e4de625f6bec5316870b99d812d47429277ae4
Piotr Wojciechowski <piotr87654@gmail.com>
2021-12-11 06:50:34 +0000

    Added Work Experience

--- script.js
+++ script.js
@@ -52,6 +52,15 @@ $('document').ready(function(){
           });  
         });
     });
+
+    $(document).on('mouseover','.item',function(){
+        $(this).css('border', '1px solid #e5e5e5', 'box-shadow', '0px 8px 16px #888')
+        $(this).css('box-shadow', '0px 8px 20px rgba(0,0,0,0.2)')
+    });
+    $(document).on('mouseout','.item',function(){
+        $(this).css('border', 'none')
+        $(this).css('box-shadow', 'none')
+    });
 });
 
 function getData(title) {
